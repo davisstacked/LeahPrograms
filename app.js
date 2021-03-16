68a81c45818aff03d2d54a2819034b3468f93670
davisstacked <brettzvi@gmail.com>
2021-03-15 23:11:50 -0700

    responsive nav fully functional

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 // SELECTORS
-const navButton = document.querySelector('.navbar');
+const navButton = document.querySelector('.navbar-toggler');
 const main = document.querySelector(".main");
+const navBar = document.querySelector(".navbar");
+console.log(navBar);
 
 console.log(main.classList);
 
 // FUNCTIONS
 function grow() {
   return main.classList.toggle("main-grow");
-  console.log(main.classList.toggle("main-grow"));
 }
 
 function shrink() {
   return main.classList.toggle("main-shrink");
 }
 
-
 // EVENT LISTENERS
 const handleGrow = navButton.addEventListener('click', () => {
-  grow();
-  shrink();
-});
+    grow();
+    shrink();
+  } 
+);
