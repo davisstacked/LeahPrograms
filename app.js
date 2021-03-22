cef3ede4b2b4ba9c5e3fd4804b4d066789703f99
davisstacked <brettzvi@gmail.com>
2021-03-22 15:51:37 -0700

    cleaned up javascript

--- app.js
+++ app.js
@@ -7,11 +7,6 @@ const navBar = document.querySelector('.navbar');
 
 // PROJECTS - PROJECTS HOVER
 const projectsContainer = document.querySelector('.projects-container');
-// const projectImageContainer = document.querySelector('.project-image-container');
-// const projectImage = document.querySelector('.project-image');
-
-// console.log(projectImage);
-// console.log(projectsContainer)
 
 // ---------------------------- FUNCTIONS ------------------------------
 // RESPONSIVE NAV FUNCTIONS
@@ -22,37 +17,6 @@ function grow() {
 function shrink() {
   return main.classList.toggle('main-shrink');
 };
-
-// PROJECTS HOVER 
-// function projectHover() {
-//   const imageContainerHover = projectImageContainer.classList.toggle('project-image-container-hover');
-//   const imageHover = projectImage.classList.toggle('project-image-hover');
-//   return {
-//     imageContainerHover,
-//     imageHover
-//   }
-// // };
-
-// function hoverProjectImageContainer(e) {
-//   const projectImageContainer = e.target.classList.contains('project-image-container');
-
-//   const projectImage = e.target.classList.contains('project-image');
-
-//   const projectImageContainerHover = projectImageContainer.classlist.toggle('project-image-container-hover');
-//   const projectImageHover = prjectImage.classList.toggle('project-image-hover')
-//   if (projectImageContainer) {
-//     return {
-//       projectHover,
-//       projectImageHover
-//     }
-//   }
-// }
-    
-// function hoverProjectImage(e) {
-//   if (e.target.classList.contains('project-image')) {
-//     e.target.classList.toggle('project-image-hover')
-//   }
-// }
 
 // PROJECTS SCROLL REVEAL
 const scroll = ScrollReveal().reveal('.project', {
@@ -74,18 +38,6 @@ const handleGrow = navButton.addEventListener('click', () => {
 );
 
 // PROJECT HOVER
-
-
-
-// const handleHover = projectsContainer.addEventListener('mouseenter', (e) => {
-//   hoverProjectImageContainer(e);
-//   // hoverProjectImage(e);
-// });
-
-// const handleRemoveHover = projectsContainer.addEventListener('mouseleave', (e) => {
-//   hoverProjectImageContainer(e);
-//   // hoverProjectImage(e);
-// });
 
 projectsContainer.addEventListener('mouseover', hoverImage);
 projectsContainer.addEventListener('mouseout', hoverImage);
